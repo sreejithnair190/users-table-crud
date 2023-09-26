ff205193d4b51e4e0798173b2d7b14834383be9b
sreejithnair190 <sreejith190nair@gmail.com>
2023-09-26 15:26:02 +0530

    removed log

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import { store } from './redux/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {console.log("app")}
     <Provider store={store}>
         <App />
     </Provider>
